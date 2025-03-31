5ef50470fd259b7babc489ed356c5c3ec38b65aa
truestayhere <malininapolina@gmail.com>
2025-03-31 16:20:32 +0300

    Initial commit: Spring Boot project structure for Meeting Scheduler

---
+++ src/test/java/com/truestayhere/meeting_scheduler/MeetingSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truestayhere.meeting_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
